d8720b3936fa0806e29628d1e9a460a262eeb002
Evan Tahler <evantahler@gmail.com>
2016-06-23 17:36:57 -0700

    better redis conf

--- config/redis.js
+++ config/redis.js
@@ -6,46 +6,35 @@ var password = process.env.REDIS_PASS || null;
 exports['default'] = {
   redis: function(api){
     var Redis;
-    if(process.env.FAKEREDIS === 'false' || process.env.REDIS_HOST !== undefined){
-      Redis = require('ioredis');
-    }else{
-      Redis = require('fakeredis');
+    var client;
+    var subscriber;
+    var tasks;
+
+    // cleanup if we are rebooting or looing in config load
+    if(api.config.redis){
+      if(api.config.redis.client){     api.config.redis.client.quit();     }
+      if(api.config.redis.subscriber){ api.config.redis.subscriber.quit(); }
+      if(api.config.redis.tasks){      api.config.redis.tasks.quit();      }
     }
 
-    // create the redis clients
-    // because we are using a cheap heroku redis with a connection limit, we'll use 1 client
-    var client     = new Redis({ port: port, host: host, password: password, db: database });
-    var subscriber = new Redis({ port: port, host: host, password: password, db: database });
+    if(process.env.FAKEREDIS === 'false' || process.env.REDIS_HOST !== undefined){
+      Redis = require('ioredis');
+      client     = new Redis({ port: port, host: host, password: password, db: database });
+      subscriber = new Redis({ port: port, host: host, password: password, db: database });
+      tasks      = new Redis({ port: port, host: host, password: password, db: database });
+    }else{
+      Redis = require('fakeredis');
+      client     = Redis.createClient(port, host, {fast: true});
+      subscriber = Redis.createClient(port, host, {fast: true});
+      tasks      = Redis.createClient(port, host, {fast: true});
+    }
 
     return {
       '_toExpand': false,
+      // create the redis clients
       client:     client,
       subscriber: subscriber,
-      tasks:      client,
+      tasks:      tasks,
     };
-  }
-};
-
-exports.test = {
-  redis: function(api){
-    if(process.env.FAKEREDIS === 'false'){
-      var Redis = require('ioredis');
-      return {
-        '_toExpand': false,
-
-        client:     new Redis({ port: port, host: host, password: password, db: database }),
-        subscriber: new Redis({ port: port, host: host, password: password, db: database }),
-        tasks:      new Redis({ port: port, host: host, password: password, db: database }),
-      };
-    }else{
-      var Redis = require('fakeredis');
-      return {
-        '_toExpand': false,
-
-        client:     Redis.createClient(port, host, {fast: true}),
-        subscriber: Redis.createClient(port, host, {fast: true}),
-        tasks:      Redis.createClient(port, host, {fast: true}),
-      };
-    }
   }
 };
