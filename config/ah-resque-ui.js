fe485b142f703160cc99d7baff4ca0d462f65bb4
Evan Tahler <evantahler@gmail.com>
2017-10-06 17:44:30 -0700

    lock resque middleware to logged-in session

--- config/ah-resque-ui.js
+++ config/ah-resque-ui.js
@@ -2,7 +2,7 @@ exports.default = {
   'ah-resque-ui': function (api) {
     return {
       // the name of the middleware which will protect all actions in this plugin
-      middleware: null
+      middleware: ['logged-in-session']
     }
   }
 }
